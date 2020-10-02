d47dd575cb1d79e59990fb0479fa6272bbcdb3b0
Mani1211 <manivasagam.shanmugam@touron.in>
2020-10-02 10:45:06 +0530

    test

--- src/api/touron.js
+++ src/api/touron.js
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://touronapi.herokuapp.com",
+  // baseURL: "https://touronapi.herokuapp.com",
+  // baseURL: "https://touronapi.herokuapp.com",
+  // baseURL: "https://touronapi.herokuapp.com",
+  // baseURL: "https://touronapi.herokuapp.com",
+  // baseURL: "https://touronapi.herokuapp.com",
 });
